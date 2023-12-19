b0de84ed40231ad139a664cc7516be1276442d15
OS Kernel <tosic.moma@gmail.com>
2023-12-19 02:13:48 -0800

    fix process pool

--- QhX/parallelization.py
+++ QhX/parallelization.py
@@ -5,6 +5,7 @@ import threading
 import time
 from multiprocessing import Pool
 from datetime import datetime
+from concurrent import futures
 
 def background_log(set_id, e : threading.Event, delta_seconds : float):
     total_time = 0
@@ -16,13 +17,14 @@ def background_log(set_id, e : threading.Event, delta_seconds : float):
     cur_time = datetime.now()
     print(f'End time for ID {set_id} : {cur_time}\n')
 
-def process1_wrapper(set_id, data_manager, delta_seconds : float, log_time : bool):
+def process1_wrapper(args):
+    set_id, data_manager, delta_seconds, log_time = args 
     e = None
     if log_time:
         e = threading.Event()
         daemon = threading.Thread(target = background_log, args = (set_id, e, delta_seconds))
         daemon.start()
-    time.sleep(20)
+    time.sleep(10)
     res = None
     #res = process1(data_manager, set_id, ntau=80, ngrid=100, provided_minfq=2000, provided_maxfq=10, include_errors=False)
     #print(data_manager)
@@ -33,18 +35,16 @@ def process1_wrapper(set_id, data_manager, delta_seconds : float, log_time : boo
 TIMEOUT = 1000.0
 
 def process_ids(setids, data_manager, num_workers = 4, delta_seconds = 14.0, log_time = True, save_results = None, chunk_size = 5):	
+    results = []
+    
+    with futures.ProcessPoolExecutor(max_workers = num_workers) as exe:
+        results = exe.map(process1_wrapper, [(setids[i], data_manager, delta_seconds, log_time) for i in range(len(setids))])
 
-    pool = Pool(processes = num_workers)
-    
-    results = pool.starmap(process1_wrapper, [(setids[i], data_manager, delta_seconds, log_time) for i in range(len(setids))], chunksize = chunk_size)
-    for result in results:
-        print(result)
-    
-    if save_results is not None:
-        try:
-            with open(save_results, 'w') as f:
-                for result in results:
-                    f.write(result + '\n')
-        except Exception as e:
-            print('Error while saving: \n'+ str(e))
+        if save_results is not None:
+            try:
+                with open(save_results, 'w') as f:
+                    for result in results:
+                        f.write(result + '\n')
+            except Exception as e:
+                print('Error while saving: \n'+ str(e))
 
