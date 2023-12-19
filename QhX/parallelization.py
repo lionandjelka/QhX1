34ac2bd1fbb9a473ffbc6a52d45a1b5ed1af6d21
OS Kernel <tosic.moma@gmail.com>
2023-12-18 23:44:38 -0800

    add parallelization prototype

--- QhX/parallelization.py
+++ QhX/parallelization.py
@@ -1,38 +1,50 @@
-# parametrize
+from QhX.detection import process1
 
 import os
-
-from tqdm.contrib.concurrent import process_map  # or import tqdm for tqdm
 import threading
+import time
+from multiprocessing import Pool
+from datetime import datetime
 
-from time import sleep
+def background_log(set_id, e : threading.Event, delta_seconds : float):
+    total_time = 0
+    cur_time = datetime.now()
+    print(f'Starting time for ID {set_id} : {cur_time}')
+    while e.wait(delta_seconds) == False:
+        total_time += delta_seconds
+        print(f'Processing {set_id}\nDuration: {total_time/delta_seconds} ticks\n{delta_seconds} seconds each\nTime so far {total_time}s\n')
+    cur_time = datetime.now()
+    print(f'End time for ID {set_id} : {cur_time}\n')
 
-DELTA_SECONDS = 15.0
-NUM_NODES = 2
-semaphore = threading.Semaphore(NUM_NODES)
-
-def background_logs(set_id, e : threading.Event):
-  total_time = 0
-  while e.wait(DELTA_SECONDS) == False:
-    total_time += DELTA_SECONDS
-    print(f'Processing {set_id}\nDuration: {total_time/DELTA_SECONDS} ticks\n{DELTA_SECONDS} seconds each\nTime so far {total_time}s\n')
-
-def process1_wrapper(set_id):
-    semaphore.acquire()
-    # No need for print, tqdm will handle the progress display
-    e = threading.Event()
-    daemon = threading.Thread(target = background_logs, args = (set_id, e))
-    daemon.start()
-    res = process1(data_manager, set_id, ntau=80, ngrid=100, provided_minfq=2000, provided_maxfq=10, include_errors=False)
-    e.set()
-    semaphore.release()
+def process1_wrapper(set_id, data_manager, delta_seconds : float, log_time : bool):
+    e = None
+    if log_time:
+        e = threading.Event()
+        daemon = threading.Thread(target = background_log, args = (set_id, e, delta_seconds))
+        daemon.start()
+    time.sleep(20)
+    res = None
+    #res = process1(data_manager, set_id, ntau=80, ngrid=100, provided_minfq=2000, provided_maxfq=10, include_errors=False)
+    #print(data_manager)
+    if log_time:
+        e.set()
     return res
 
-setids = ['1384177', '1384184']
+TIMEOUT = 1000.0
 
-process1_results = process_map(process1_wrapper, setids)
+def process_ids(setids, data_manager, num_workers = 4, delta_seconds = 14.0, log_time = True, save_results = None, chunk_size = 5):	
 
-# Results are already in process1_results, you can process or print them as needed
-for result in process1_results:
-    print(result)
+    pool = Pool(processes = num_workers)
+    
+    results = pool.starmap(process1_wrapper, [(setids[i], data_manager, delta_seconds, log_time) for i in range(len(setids))], chunksize = chunk_size)
+    for result in results:
+        print(result)
+    
+    if save_results is not None:
+        try:
+            with open(save_results, 'w') as f:
+                for result in results:
+                    f.write(result + '\n')
+        except Exception as e:
+            print('Error while saving: \n'+ str(e))
 
